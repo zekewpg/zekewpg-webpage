729f5042bba5315dbff32e2d178a974494506e00
zekewpg <8922590+zekewpg@users.noreply.github.com>
2026-07-06 06:32:58 -0500

    added code to ps script

--- Generate-ImagesJS.ps1
+++ Generate-ImagesJS.ps1
@@ -1,3 +1,23 @@
+$Folder = "D:\Rob's Documents\GitRepositories\zekewpg-webpage\images"
+
+# Get all files (change the filter if needed)
+$Files = Get-ChildItem -Path $Folder -File | Sort-Object Name
+
+$Counter = 1
+
+foreach ($File in $Files) {
+    $NewName = "{0:D3}{1}" -f $Counter, $File.Extension.ToLower()
+
+    Rename-Item -Path $File.FullName -NewName $NewName
+
+    Write-Host "$($File.Name) -> $NewName"
+
+    $Counter++
+}
+
+Write-Host "`nDone!"
+
+
 # Folder containing your images
 $imageFolder = "images"
 
